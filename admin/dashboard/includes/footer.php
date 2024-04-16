cd694cc18498a7fffbb6b9e1c26821599bec158c
089u97t <maryanthonytochukwu@gmail.com>
2024-04-16 04:55:35 -0700

    first commit

---
+++ admin/dashboard/includes/footer.php
@@ -0,0 +1,3 @@
+<footer class="main-footer text-center">
+    <i>&copy; <?php echo date("Y"); ?> . <a href="https://topupmom/welcome">Jossyfeydata Rights Reserved.</a></i>
+</footer>
---
+++ core/helpers/vendor/site.php
@@ -0,0 +1,9 @@
+<?php
+
+    //Configuration Settings
+    $sitename= "JOSSYFEYDATA";
+    $parentdirectory = "";
+    $secret_key="0c2766f5868ec65d29f2a08059f2bdab";
+    $public_key="0c2766f5868ec65d29f2a08059f2bdab";
+   
+?>
